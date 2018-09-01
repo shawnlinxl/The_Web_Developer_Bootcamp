7975e579f899ab1b05815f1d709d68a18936b234
Shawn Lin <xiaoliang.lin@columbia.edu>
2018-08-31 23:38:26 -0400

    Initial commit.

---
+++ 95_JS_Stalker/95_stalker.js
@@ -0,0 +1,8 @@
+// Prompts to ask for inputs that will be printed out
+var first_name = prompt("What is your first name?");
+var last_name = prompt("What is your last name?");
+var age = prompt("What is your age?");
+
+// Print out user inputs in console
+console.log("Your full name is " + first_name + " " + last_name + ".");
+console.log("You are ", age, " years old.");
