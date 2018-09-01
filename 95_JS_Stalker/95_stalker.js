64a6b984aa185df490a3096c6971c995ea0158e9
Shawn Lin <xiaoliang.lin@columbia.edu>
2018-08-31 23:46:26 -0400

    Bux fix and tidy up
    * Fixed "," bug in age log.
    * Grouped first_name and last_name first before concatenation.

--- 95_JS_Stalker/95_stalker.js
+++ 95_JS_Stalker/95_stalker.js
@@ -3,6 +3,8 @@ var first_name = prompt("What is your first name?");
 var last_name = prompt("What is your last name?");
 var age = prompt("What is your age?");
 
+var full_name = first_name + " " + last_name;
+
 // Print out user inputs in console
-console.log("Your full name is " + first_name + " " + last_name + ".");
-console.log("You are ", age, " years old.");
+console.log("Your full name is " + full_name + ".");
+console.log("You are " + age + " years old.");
